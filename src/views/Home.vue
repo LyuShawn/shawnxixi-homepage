819ba00f832368a3a5bc6783b736d24ddc0aa119
Savona <905899183@qq.com>
2021-06-15 21:28:07 +0800

    [Init] 首页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello World!</h1>
+    <div class="record">
+      <a href="https://beian.miit.gov.cn" target="_blank">闽ICP备2021006317号</a>
+      <div style="width:300px;margin:0 auto; padding:20px 0;">
+        <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=35012102500342" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
+          <img src="https://gitee.com/savona/MyImageBed/raw/master/img/20210615203140.png" style="float:left;"/>
+          <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">闽公网安备 35012102500342号
+          </p>
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style scoped>
+.record {
+  
+}
+</style>
