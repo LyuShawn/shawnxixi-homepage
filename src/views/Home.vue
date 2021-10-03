e623b162c047e8ba776dae779c926b1b964c967a
Savona <905899183@qq.com>
2021-10-03 21:17:14 +0800

    [Updated] 更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,5 @@
 <template>
   <div class="home">
-    <h1>Hello World!</h1>
-    <div class="record">
-      <a href="https://beian.miit.gov.cn" target="_blank">闽ICP备2021006317号</a>
-      <div style="width:300px;margin:0 auto; padding:20px 0;">
-        <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=35012102500342" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
-          <img src="https://gitee.com/savona/MyImageBed/raw/master/img/20210615203140.png" style="float:left;"/>
-          <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">闽公网安备 35012102500342号
-          </p>
-        </a>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -18,9 +7,12 @@
 // @ is an alias to /src
 
 export default {
-  name: 'Home',
+  name: 'Home1',
+  created () {
+      console.log('这里是肖恩嘻嘻首页的控制台');
+  },
   components: {
-  }
+  },
 }
 </script>
 
