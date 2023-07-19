e9414aaef610ac3765e2e0b4da68218ee224c722
LyuShawn <lvlalala@qq.com>
2023-07-19 22:03:12 +0800

    [Update]更新首页

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="nav">
-  <h1>Hello World!</h1>
+  <h1>这里是肖恩嘻嘻的首页</h1>
     <div class="record">
       <a href="https://beian.miit.gov.cn" target="_blank">闽ICP备2021006317号</a>
       <div style="width:300px;margin:0 auto; padding:20px 0;">
         <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=35012102500342" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
-          <img src="https://gitee.com/savona/MyImageBed/raw/master/img/20210615203140.png" style="float:left;"/>
+          <img src="https://lyu-oss.oss-cn-beijing.aliyuncs.com/img-bed/%E5%A4%87%E6%A1%88%E5%9B%BE%E6%A0%87.png" style="float:left;"/>
           <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">闽公网安备 35012102500342号
           </p>
         </a>
